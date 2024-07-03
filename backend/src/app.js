21d0bc7ae1d439d7094f705ebe48b83572986d19
Kaif Shaikh <kaifshaikh2013.sk@gmail.com>
2024-07-04 02:26:45 +0530

    solved the cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,22 +9,33 @@ import billRouter from "./routes/Bill.js";
 import customerRouter from "./routes/Customer.js";
 import transactionRouter from "./routes/Transaction.js";
 import dailyReportRouter from "./routes/DailyReport.js";
+
 dotenv.config({
   path: "./src/config/config.env",
 });
 
 const app = express();
 app.use(cookieParser());
-
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://dreamy-cuchufli-66a35d.netlify.app",
+];
+
 app.use(
   cors({
+    origin: function (origin, callback) {
+      if (allowedOrigins.includes(origin) || !origin) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
-    //  origin: "https://dreamy-cuchufli-66a35d.netlify.app/"
-    origin: "http://localhost:5173",
   })
 );
-app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1", userRouter);
 app.use("/api/v1", productRouter);
